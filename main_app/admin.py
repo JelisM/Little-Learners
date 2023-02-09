139a923675c1bf5488645663c68f0f408d7fa11b
JelisM <jelismaiara@gmail.com>
2023-02-09 18:41:12 -0500

    admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Teacher, Guardian, Child
 
 #Register models
-admin.site.register(Teacher, Guardian, Child)
+admin.site.register(Teacher)
+admin.site.register(Guardian)
+admin.site.register(Child)
 
 # Register your models here.
