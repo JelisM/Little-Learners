8cdde7044cdfc0bc030a8c2bb0bc46ffbf2a7db8
mar69287 <marcoa.ruiz97@gmail.com>
2023-02-10 10:11:07 -0800

    log out redirect

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path("teachers/", views.teachers_index, name='teachers_index'),
+    # path("guardians/", views.guardians_index, name='teachers_index'),
     path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
